f94d691a670b6061862789a03e91ff7130c49745
criminus <neculaianisor@gmail.com>
2022-11-02 18:41:36 +0000

    while loop explained

--- variables.php
+++ variables.php
@@ -1,3 +1,24 @@
 <?php
 
-echo "test";
+/* Rules for PHP variables:
+    A variable starts with the $ sign, followed by the name of the variable.
+    A variable name must start with a letter or the underscore character.
+    A variable name cannot start with a number.
+    A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+    Variable names are case-sensitive ($age and $AGE are two different variables).
+*/
+
+$myVar = "This is a text."; // This is a string (which means it will be read as text)
+/* 
+You can use single quotes or double quotes to assign a value to a variable.
+But never start a string with single quote and end it with double quotes or vice versa.
+*/
+
+//Let's echo the first variable.
+echo $myVar;
+echo "<br />";
+
+$mySecondVar = 15; // this is a whole number
+$myThirdVar = 10.5; // this is a float number
+
+//Can we use numbers as strings (double quotes or single quotes)? Yes. But they will be treated as texts not numbers.
---
+++ while.php
@@ -0,0 +1,38 @@
+<?php
+//While loop examples
+//A while loop will execute itself until the condition is true.
+
+$myNumber = 4;
+$target = 10;
+while($myNumber < $target) {
+    echo "$myNumber is less than $target<br />";
+
+    //Add a number to $myNumber until it reaches 9.
+    //It will reach 9 because the condition must be true.
+    //So 9 is less than 10 and it will stop at 9.
+    $myNumber++;
+}
+
+//Another way of adding a number to the first variable:
+while($myNumber < $target) {
+    echo "$myNumber is less than $target<br />";
+    //Add a number to $myNumber until it reaches 9.
+    //It will reach 9 because the condition must be true.
+    //So 9 is less than 10 and it will stop at 9.
+    $myNumber = $myNumber + 1;
+}
+
+/* Infinite loop
+An infinite loop will run forever and that's because
+the condition will be always true.
+
+Example:
+
+while(4 < 5) {
+    echo "Run this forever.";
+}
+
+The loop will run forever because the statement is true. 
+4 will always be less than 5.
+This works with all Comparison Operators as long as the condition is true.
+*/
