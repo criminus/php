9f2269e4c0a2dab828130ba404f3769e7c76e19a
criminus <neculaianisor@gmail.com>
2022-11-02 20:06:48 +0000

    foreach

---
+++ foreach.php
@@ -0,0 +1,16 @@
+<?php
+//With foreach we can loop through arrays and print out values.
+
+$numbers = [0, 15, 22, 55];
+foreach ($numbers as $number) {
+    echo $number . "<br />";
+}
+
+echo "<br />";
+
+//Can we use the same logic as the while loop to count? Yes
+//Let's grab the original value and add +1 for each one in the array.
+foreach ($numbers as $number) {
+    $number++;
+    echo "The new value for each index is: $number<br />";
+}
