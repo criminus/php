bd3940a56ab9a24a612f4d14c2a7264b724bcdbb
criminus <neculaianisor@gmail.com>
2022-11-06 01:01:07 +0000

    strings + simple form with basic checks

--- functions/functions_strings.php
+++ functions/functions_strings.php
@@ -1,5 +1,4 @@
 <?php
-// https://www.php.net/ref.strings
 
 $myString = "how long is this string?";
 echo strlen($myString); // <-- Will print 24 / including empty spaces.
@@ -10,3 +9,7 @@ $myString2 = "how many words are in this string?";
 echo str_word_count($myString2); // <-- Will print 7 because there are 7 words separated by spaces.
 
 echo "<br />";
+
+
+$sim = similar_text('bafoobar', 'barfoo', $perc);
+echo "similarity: $sim ($perc %)\n";
